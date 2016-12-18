8131903ef607437843ae61967cc4f0b3add34a8b
unknown <vihka.spihka@gmail.com>
2016-12-19 00:39:18 +0300

    Non-animated 1st + 2nd + 3rd + additionalPlus #1 + #2

--- js/plugin.js
+++ js/plugin.js
@@ -2,22 +2,22 @@
 
 $(document).ready(function() {
 	
-	// Remember initial order
-	var firstOrderArray = []; 
-	for(var i = 0; i < $('.taskList__item').length; i++) {
-		
-		firstOrderArray[i] = $('.taskList__item').eq(i).text();
-	}
-	// console.log(firstOrderArray);
-
 	var itemsCount = $('.taskList__item').length;
 
 	var debugMode = $('input').filter('[type="checkbox"]').filter('#controlPanel__checkboxDebug');
 
+	// Remember initial order
+	var firstOrderArray = []; 
+	for(let i = 0; i < itemsCount; i++) {
+		
+		firstOrderArray[i] = $('.taskList__item').eq(i).text();
+	}
+
 	$('.tasks__taskList').sortable();
+
 	$('.tasks__taskList').disableSelection();;
 
-	// Event handler - click on task
+// Event handler - click on task
 	$(document).on('click', 'ul *', function(event){
 		event.stopPropagation();
 
@@ -26,9 +26,9 @@ $(document).ready(function() {
 		var directionChoice = $('input').filter('[name="direction"]').filter(':checked').attr('value');
 		
 		switch(directionChoice) {
-			case 'up': $listItemClicked.makeFirstByClick(); break;
-			case 'down': $listItemClicked.makeLastByClick(); break;
-			default: $listItemClicked.makeFirstByClick();
+			case 'up': $listItemClicked.upByClick(); break;
+			case 'down': $listItemClicked.downByClick(); break;
+			default: $listItemClicked.upByClick();
 		}
 
 		// Output current order in the console if use Debug mode
@@ -37,19 +37,51 @@ $(document).ready(function() {
 		}
 	});
 
-	// Event handler - click into Reset button
-	$('#resetBtn').bind('click', firstOrderArray, function() {
-		$('.tasks__taskList').text(firstOrderArray.text());
+// Event handler - click into Resize Button
+	$('#sizeAutoBtn').on('click', function() {
+
+		for (let i = 0; i < itemsCount; i++) {
+			
+			$('.taskList__item').eq(i).css('height', 'auto');
+		}
+	});
+
+// Event handler - click into Resize Button to standart height
+	$('#sizeStandartBtn').on('click', function() {
+
+		for (let i = 0; i < itemsCount; i++) {
+			
+			$('.taskList__item').eq(i).css('height', '55');
+		}
+	});
+
+// Event handler - click into Reset button (like revert() function in task)
+	$('#resetBtn').on('click', function() {
+			
+		var itemText;
+
+		for(let i = 0; i < itemsCount; i++) {
+
+			$('.taskList__item').eq(i).empty();
+			itemText = firstOrderArray[i];
+			$('.taskList__item').eq(i).text(itemText);
+		}
 	});
 });
 
 // Plugin that move task up in list
 (function($){
-	$.fn.makeFirstByClick = function () {
+	$.fn.upByClick = function () {
 		var number = $('input').filter('[type="number"]').val();
 
 		if(this.index() > number) {
 			$('.mistake').empty();
+			this.addClass('moveUp');
+			// this.animate({
+			// 	left: 200
+			// }, 800, function(){
+			// 	console.log('animation up');
+			// });
 			this.insertBefore($('.taskList__item').eq(number));
 		}
 		else {
@@ -60,7 +92,7 @@ $(document).ready(function() {
 
 // Plugin that move task down in list
 (function($){
-	$.fn.makeLastByClick = function () {
+	$.fn.downByClick = function () {
 		var number = $('input').filter('[type="number"]').val();
 		
 		if(this.index() < number) {
@@ -72,3 +104,29 @@ $(document).ready(function() {
 		}
 	}
 })(jQuery);
+
+// Doesn`t work - crop text function
+function cropText () {
+
+	let item = $('.taskList__item').eq(2);
+		text = item.text(),
+		height = item.height(),
+		clone = item.clone();
+	
+	clone.css({
+			position: 'absolute',
+			visibility: 'hidden',
+			height: 'auto'
+		});
+
+	clone.insertAfter(item);
+
+	var l = text.length - 1;
+		for (; l >= 0 && clone.height() > height; --l) {
+			clone.text(text.substring(0, l) + '...');
+		}
+	var cloneText = clone.text();
+	console.log(cloneText)
+		clone.remove();
+	return cloneText;
+}
